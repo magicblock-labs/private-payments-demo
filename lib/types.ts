c803d52557a9cce5d1db5ce71531266bfb51f5ff
Dodecahedr0x <hexadecifish@gmail.com>
2026-01-28 18:57:40 +0100

    fix: build

--- lib/types.ts
+++ lib/types.ts
@@ -1,8 +1,3 @@
-import { PrivatePayments } from '@/program/private_payments';
-import { IdlAccounts } from '@coral-xyz/anchor';
-
-export type DepositAccount = IdlAccounts<PrivatePayments>['deposit'];
-
 export interface TokenListEntry {
   mint: string;
   creator: string;
