96ead7bb3315cc1de2ece9f8118f6e23f77bfd1e
Dodecahedr0x <hexadecifish@gmail.com>
2026-01-31 13:54:09 +0100

    fint: lint

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,7 +8,8 @@ export default function Navbar() {
   const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
-    setMounted(true);
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    void setMounted(true);
   }, []);
 
   return (
@@ -16,7 +17,9 @@ export default function Navbar() {
       <NavigationMenuList suppressHydrationWarning>
         <NavigationMenuItem suppressHydrationWarning>
           <div className='w-fit' suppressHydrationWarning>
-            {mounted && <WalletMultiButton className='bg-linear-to-r! from-blue-600! to-purple-600! border-0! rounded-lg! px-6! py-2! text-white! font-medium! transition-all! duration-300! hover:from-blue-700! hover:to-purple-700! hover:shadow-lg! hover:scale-105!' />}
+            {mounted && (
+              <WalletMultiButton className='bg-linear-to-r! from-blue-600! to-purple-600! border-0! rounded-lg! px-6! py-2! text-white! font-medium! transition-all! duration-300! hover:from-blue-700! hover:to-purple-700! hover:shadow-lg! hover:scale-105!' />
+            )}
           </div>
         </NavigationMenuItem>
       </NavigationMenuList>
