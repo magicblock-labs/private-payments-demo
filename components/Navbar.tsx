b968e5728f023a41c291584664da746a376dbb74
jonasXchen <jonas002@hotmail.de>
2026-01-31 15:20:22 +0800

    fix: hydration when changing wallets without manual refresh

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,22 @@
 'use client';
 
+import { useEffect, useState } from 'react';
 import { NavigationMenu, NavigationMenuItem, NavigationMenuList } from './ui/navigation-menu';
 import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 
 export default function Navbar() {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
   return (
     <NavigationMenu suppressHydrationWarning>
       <NavigationMenuList suppressHydrationWarning>
         <NavigationMenuItem suppressHydrationWarning>
           <div className='w-fit' suppressHydrationWarning>
-            <WalletMultiButton className='bg-linear-to-r! from-blue-600! to-purple-600! border-0! rounded-lg! px-6! py-2! text-white! font-medium! transition-all! duration-300! hover:from-blue-700! hover:to-purple-700! hover:shadow-lg! hover:scale-105!' />
+            {mounted && <WalletMultiButton className='bg-linear-to-r! from-blue-600! to-purple-600! border-0! rounded-lg! px-6! py-2! text-white! font-medium! transition-all! duration-300! hover:from-blue-700! hover:to-purple-700! hover:shadow-lg! hover:scale-105!' />}
           </div>
         </NavigationMenuItem>
       </NavigationMenuList>
