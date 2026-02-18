dfe3ddcd47f59035a45589d386130dab767f7e2f
Dodecahedr0x <hexadecifish@gmail.com>
2026-02-18 19:13:54 +0100

    feat: reduce quote retries

--- hooks/use-tdx-quote-verification.ts
+++ hooks/use-tdx-quote-verification.ts
@@ -1,10 +1,12 @@
 import { EPHEMERAL_RPC_URL } from '../lib/constants';
+import { logger } from '@/lib/log';
 import { verifyTeeRpcIntegrity } from '@magicblock-labs/ephemeral-rollups-sdk';
 import { useCallback, useEffect, useState } from 'react';
 
 export function useTdxQuoteVerification() {
   const [isVerified, setIsVerified] = useState<boolean>(false);
   const [isLoading, setIsLoading] = useState(false);
+  const [tries, setTries] = useState(0);
 
   const verifyQuote = useCallback(async () => {
     if (isLoading) {
@@ -12,32 +14,29 @@ export function useTdxQuoteVerification() {
     }
 
     setIsLoading(true);
+    setTries(old => old + 1);
 
     try {
       const isVerified = await verifyTeeRpcIntegrity(EPHEMERAL_RPC_URL);
       setIsVerified(isVerified);
     } catch (error) {
-      console.error('Error verifying quote:', error);
+      logger.error('Error verifying quote:', error);
       setIsVerified(false);
     } finally {
       setIsLoading(false);
     }
   }, [isLoading]);
 
-  const resetVerification = useCallback(() => {
-    verifyQuote();
-  }, [verifyQuote]);
-
   useEffect(() => {
     const interval = setInterval(() => {
-      if (!isVerified && !isLoading) {
+      if (!isVerified && !isLoading && tries < 3) {
         // Initial verification
         verifyQuote();
       }
     }, 3000);
 
     return () => clearInterval(interval);
-  }, [isVerified, isLoading, verifyQuote]);
+  }, [isVerified, isLoading, verifyQuote, tries]);
 
-  return { isVerified, isLoading, resetVerification };
+  return { isVerified, isLoading, verifyQuote };
 }
